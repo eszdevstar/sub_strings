fe951d24b34016388de17f6b9aaf8d643e9f97ef
eszdevstar <devstaresz@gmail.com>
2022-12-26 20:22:57 -0300

    finish proj

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
-puts "Hello, World!!!"
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(input, dictionary)
+  dictionary.reduce(Hash.new(0)) do |output, word|
+    output[word] = input.downcase.scan(word).length if input.downcase.include?(word)
+    output
+  end
+end
+
+p substrings("below", dictionary)
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
